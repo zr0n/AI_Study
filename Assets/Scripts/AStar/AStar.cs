ff4082fd4b19fc1f3e6e79ed0ba5ce2a2d1788f2
Luiz Fernando <fernandoaff1@gmail.com>
2019-03-26 05:28:37 -0300

    Add AStar Pathfinding

---
+++ Assets/Scripts/AStar/AStar.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+
+    public class AStar : MonoBehaviour
+    {
+        public int rows = 10, cols = 10;
+        public Line lineTemplate;
+
+        public float cellSize = 100;
+        public int frameRate = 1;
+
+        public GameObject cellOccupied;
+        public GameObject cellVisited;
+        public GameObject cellEmpty;
+        public GameObject cellPossible;
+
+        public Vector2 spriteScale = new Vector2(10, 10);
+
+        public float spritePositionFactor = 1;
+        public Vector2 spriteGridOffset = new Vector2(5f, 5f);
+
+        public List<Vector2> blockers = new List<Vector2>();
+
+
+        public List<Cell> grid = new List<Cell>();
+
+        List<Cell> closedSet = new List<Cell>();
+        List<Cell> openSet = new List<Cell>();
+        List<Cell> pathFound = new List<Cell>();
+        Dictionary<Cell, Cell> cameFrom = new Dictionary<Cell, Cell>();
+
+
+        int lowestScore = int.MaxValue;
+
+        Cell from;
+        Cell to;
+
+        bool foundPath;
+        
+        
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Application.targetFrameRate = frameRate;
+            SetupGrid();
+            from = grid[0];
+            to = grid[grid.Count - 1];
+            openSet.Add(from);
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (foundPath)
+                return;
+
+            if (openSet.Count > 0)
+            {
+                Cell current = GetLowestScoreOpen();
+
+                DrawBackground(current, CellStatus.Occupied);
+
+                if (current == to)
+                {
+                    foundPath = true;
+                    pathFound = ReconstructPath(current);
+                    Debug.Log("Found Path");
+                    DrawPathFound();
+                    return;
+                }
+                openSet.Remove(current);
+                closedSet.Add(current);
+
+                List<Cell> neighbors = current.GetNeighborsAStar();
+                foreach (Cell neighbor in neighbors)
+                {
+                    if (closedSet.Contains(neighbor))
+                        continue;
+
+                    int tentativeScore = current.gScore + DistanceBetween(current, neighbor);
+
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
+                    else if (tentativeScore >= neighbor.gScore)
+                        continue;
+
+                    cameFrom[neighbor] = current;
+                    neighbor.gScore = tentativeScore;
+                    neighbor.fScore = neighbor.gScore + DistanceBetween(neighbor, to);
+                }
+
+            }
+        }
+        Cell GetLowestScoreOpen()
+        {
+            int lowest = int.MaxValue;
+            Cell lCell = null;
+            foreach(Cell cell in openSet)
+            {
+                if (cell.fScore <= lowest)
+                {
+                    lowest = cell.fScore;
+                    lCell = cell;
+                }
+            }
+            return lCell;
+        }
+        List<Cell> ReconstructPath(Cell current)
+        {
+            List<Cell> totalPath = new List<Cell>{ current };
+
+            while(cameFrom.ContainsKey(current))
+            {
+                current = cameFrom[current];
+                totalPath.Add(current);
+            }
+
+            return totalPath;
+        }
+
+
+        public static AStar Build(GameObject template)
+        {
+            return GameObject.Instantiate(template).GetComponent<AStar>();
+        }
+
+        void SetupGrid()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Vector2 pos = new Vector2(j, i);
+                    Cell newCell = new Cell(i, j, this);
+                    newCell.visited = blockers.Contains(pos);
+                    DrawBackground(newCell, newCell.visited ? CellStatus.Visited : CellStatus.Empty);
+                    grid.Add(newCell);
+                }
+            }
+            grid[0].visited = true;
+
+        }
+        int DistanceBetween(Cell a, Cell b)
+        {
+            return Mathf.FloorToInt((a.Position - b.Position).magnitude);
+        }
+        public void DrawPathFound()
+        {
+            foreach(Cell cell in grid)
+            {
+                if (pathFound.Contains(cell))
+                {
+                    DrawBackground(cell, CellStatus.Occupied);
+                }
+                else if(!cell.visited)
+                {
+                    DrawBackground(cell);
+                }
+            }
+        }
+        public void DrawBackground(Cell cell, CellStatus status = CellStatus.Empty)
+        {
+            GameObject objectToSpawn = GetSpriteToSpawn(status);
+            if (objectToSpawn == null)
+                return;
+
+            GameObject spawnedObject = Instantiate(objectToSpawn);
+
+            Vector3 worldPosition = new Vector3();
+            worldPosition.x = (cell.x * spritePositionFactor) + spriteGridOffset.x;
+            worldPosition.y = (cell.y * spritePositionFactor) + spriteGridOffset.y;
+
+            spawnedObject.transform.position = worldPosition;
+            spawnedObject.transform.localScale = spriteScale;
+
+            cell.SwapSprite(spawnedObject);
+
+        }
+        GameObject GetSpriteToSpawn(CellStatus status = CellStatus.Empty)
+        {
+            switch (status)
+            {
+                case CellStatus.Visited:
+                    return cellVisited;
+                case CellStatus.Occupied:
+                    return cellOccupied;
+                case CellStatus.Possible:
+                    return cellPossible;
+                default:
+                    return cellEmpty;
+            }
+        }
+
+        public int GetCellIndex(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= cols || y >= rows)
+                return -1;
+
+            return (x + y * rows);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Global/Cell.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+
+    public class Cell
+    {
+        public int x, y;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(this.x, this.y); ;
+            }
+        }
+
+        public bool[] walls = { true, true, true, true }; //top, right, bottom, left
+
+        float cellSize;
+        public bool visited;
+        MazeGenerator maze;
+        AStar aStar;
+        GameObject currentSprite;
+        List<Line> wallsRendered = new List<Line>();
+
+        const int TOP = 0, RIGHT = 1, BOTTOM = 2, LEFT = 3;
+
+        Line lineTemplate;
+
+        /* A Star Search Variables */
+        public int gScore = int.MaxValue;
+        public int fScore = int.MaxValue;
+        public Cell cameFrom;
+
+
+        public Cell(int x, int y, MazeGenerator maze)
+        {
+            this.cellSize = maze.cellSize;
+            this.x = x;
+            this.y = y;
+            
+            this.maze = maze;
+            //RandomWalls();
+            lineTemplate = maze.lineTemplate;
+            DrawWalls();
+            maze.DrawBackground(this);
+
+
+        }
+        public Cell(int x, int y, AStar aStar)
+        {
+            this.cellSize = aStar.cellSize;
+            this.x = x;
+            this.y = y;
+            this.aStar = aStar;
+            lineTemplate = aStar.lineTemplate;
+            DrawWalls();
+            aStar.DrawBackground(this);
+
+
+        }
+       
+        public void Redraw()
+        {
+            ClearAllLines();
+            DrawWalls();
+        }
+        public void ClearAllLines()
+        {
+            foreach (Line line in wallsRendered)
+            {
+                if (line && line.gameObject)
+                    GameObject.Destroy(line.gameObject);
+            }
+        }
+        void RandomWalls()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                walls[i] = Random.Range(1, 10) % 2 == 0;
+            }
+        }
+
+        public void SwapSprite(GameObject newSprite)
+        {
+            if (currentSprite != null)
+                GameObject.Destroy(currentSprite);
+
+            currentSprite = newSprite;
+        }
+
+
+        public int GetCellIndex(int i, int j)
+        {
+            if (maze != null)
+                return maze.GetCellIndex(j, i);
+            else if (aStar != null)
+                return aStar.GetCellIndex(j, i);
+            else
+                return -1;
+        }
+
+        public void RemoveWallBetween(Cell neighborCell)
+        {
+            Cell a = this;
+            Cell b = neighborCell;
+
+            int i = a.x - b.x;
+            int j = a.y - b.y;
+            if (i == -1)
+            {
+                a.walls[LEFT] = false;
+                b.walls[RIGHT] = false;
+            }
+            else if (i == 1)
+            {
+                a.walls[RIGHT] = false;
+                b.walls[LEFT] = false;
+            }
+
+            else if (j == -1)
+            {
+                a.walls[TOP] = false;
+                b.walls[BOTTOM] = false;
+            }
+            else if (j == 1)
+            {
+                a.walls[BOTTOM] = false;
+                b.walls[TOP] = false;
+            }
+            Debug.Log("i: " + i + " j: " + j);
+            a.Redraw();
+            b.Redraw();
+        }
+
+        public List<Cell> GetNeighbors()
+        {
+            List<Cell> allNeighbors = new List<Cell>();
+            List<Cell> unvisitedNeighbors = new List<Cell>();
+
+            int iTop = GetCellIndex(x, y + 1);
+            int iRight = GetCellIndex(x + 1, y);
+            int iBottom = GetCellIndex(x, y - 1);
+            int iLeft = GetCellIndex(x - 1, y);
+
+            if (maze)
+            {
+                if (iTop > -1)
+                    allNeighbors.Add(maze.grid[iTop]);
+                if (iRight > -1)
+                    allNeighbors.Add(maze.grid[iRight]);
+                if (iBottom > -1)
+                    allNeighbors.Add(maze.grid[iBottom]);
+                if (iLeft > -1)
+                    allNeighbors.Add(maze.grid[iLeft]);
+            }
+            
+
+
+            foreach (var neighbor in allNeighbors)
+            {
+                if (!neighbor.visited)
+                {
+                    unvisitedNeighbors.Add(neighbor);
+                    if (maze)
+                        maze.DrawBackground(neighbor, neighbor.visited ? CellStatus.Visited : CellStatus.Empty);
+                    
+                }
+            }
+
+            return unvisitedNeighbors;
+        }
+
+        public List<Cell> GetNeighborsAStar()
+        {
+            int iTop = GetCellIndex(x, y + 1);
+            int iRight = GetCellIndex(x + 1, y);
+            int iBottom = GetCellIndex(x, y - 1);
+            int iLeft = GetCellIndex(x - 1, y);
+
+            List<Cell> allNeighbors = new List<Cell>();
+            List<Cell> validNeighbors = new List<Cell>();
+            if (aStar)
+            {
+                if (iTop > -1)
+                    allNeighbors.Add(aStar.grid[iTop]);
+                if (iRight > -1)
+                    allNeighbors.Add(aStar.grid[iRight]);
+                if (iBottom > -1)
+                    allNeighbors.Add(aStar.grid[iBottom]);
+                if (iLeft > -1)
+                    allNeighbors.Add(aStar.grid[iLeft]);
+            }
+
+            foreach(Cell neighbor in allNeighbors)
+            {
+                if (!neighbor.visited)
+                {
+                    validNeighbors.Add(neighbor);
+                }
+            }
+            return validNeighbors;
+        }
+
+        public void DrawWalls()
+        {
+            if (walls[0])
+            {
+                Line newLine = GameObject.Instantiate<Line>(lineTemplate);
+                newLine.a = new Vector2((x * cellSize), (y * cellSize) + cellSize);
+                newLine.b = new Vector2((x * cellSize) + cellSize, (y * cellSize) + cellSize);
+                newLine.Draw();
+                wallsRendered.Add(newLine);
+            }
+            if (walls[1])
+            {
+                Line newLine = GameObject.Instantiate<Line>(lineTemplate);
+                newLine.a = new Vector2(x * cellSize, y * cellSize);
+                newLine.b = new Vector2(x * cellSize, (y * cellSize) + cellSize);
+                newLine.Draw();
+                wallsRendered.Add(newLine);
+
+            }
+            if (walls[2])
+            {
+                Line newLine = GameObject.Instantiate<Line>(lineTemplate);
+                newLine.a = new Vector2(x * cellSize, y * cellSize);
+                newLine.b = new Vector2(x * cellSize + cellSize, (y * cellSize));
+                newLine.Draw();
+                wallsRendered.Add(newLine);
+
+            }
+            if (walls[3])
+            {
+                Line newLine = GameObject.Instantiate<Line>(lineTemplate);
+                newLine.a = new Vector2((x * cellSize) + cellSize, (y * cellSize));
+                newLine.b = new Vector2((x * cellSize) + cellSize, (y * cellSize) + cellSize);
+                newLine.Draw();
+                wallsRendered.Add(newLine);
+            }
+        }
+
+
+
+
+    }
+
+    public enum CellStatus
+    {
+        Empty,
+        Occupied,
+        Visited,
+        Possible
+    }
+
+
+}
